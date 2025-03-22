1811417202399f3c699dd10d9f19acd6e2775b14
Kleyton L Silva <156526710+KtonSilva@users.noreply.github.com>
2025-03-22 18:36:34 -0300

    Update ProductList.mjs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,28 +1,30 @@
-import { renderListWithTemplate } from './utils.mjs';
+import { products } from "./ProductData.mjs";
 
-function productCardTemplate(product) {
-    return `<li class="product-card">
-      <a href="product_pages/?product=${product.Id}">
-        <img src="${product.Image}" alt="Image of ${product.Name}">
-        <h2 class="card__brand">${product.Brand.Name}</h2>
-        <h3 class="card__name">${product.Name}</h3>
-        <p class="product-card__price">$${product.FinalPrice}</p>
-      </a>
-    </li>`;
-  }
+class ProductList {
+    constructor(containerId) {
+        this.container = document.getElementById(containerId);
+        this.render();
+    }
 
-export default class ProductList {
-  constructor(category, dataSource, listElement) {
-    this.category = category;
-    this.dataSource = dataSource;
-    this.listElement = listElement;
-  }
-  async init() {
-    const list = await this.dataSource.getData();
-    this.renderList(list);
-  }
-  renderList(list) {
-    const htmlStrings = list.map(this.productCardTemplate).join('');
-    this.listElement.insertAdjacentHTML('afterbegin', htmlStrings);
-  }
+    render() {
+        if (!this.container) {
+            console.error("Container not found");
+            return;
+        }
+        
+        this.container.innerHTML = "";
+        products.forEach(product => {
+            const productElement = document.createElement("div");
+            productElement.classList.add("product-item");
+            productElement.innerHTML = `
+                <h3>${product.name}</h3>
+                <p>${product.description}</p>
+                <p><strong>Price:</strong> $${product.price.toFixed(2)}</p>
+                <button class="add-to-cart" data-id="${product.id}">Add to Cart</button>
+            `;
+            this.container.appendChild(productElement);
+        });
+    }
 }
+
+export default ProductList;
